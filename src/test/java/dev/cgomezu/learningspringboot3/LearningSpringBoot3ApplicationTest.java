d4f08599dfd699daba8b22cfe7da58462a59c04f
Carlos Alberto Gomez Uribe <cgomezu@sura.com.co>
2023-09-04 09:15:11 -0500

    Initial project commit
    add scheleton for maven project

---
+++ src/test/java/dev/cgomezu/learningspringboot3/LearningSpringBoot3ApplicationTest.java
@@ -0,0 +1,12 @@
+package dev.cgomezu.learningspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LearningSpringBoot3ApplicationTest {
+
+    @Test
+	void contextLoads() {
+	}
+}
