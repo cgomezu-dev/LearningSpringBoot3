e9eb61af8cf83e1fbf582ba3319eb2fc2e2d1a79
Carlos Alberto Gomez Uribe <cgomezu@sura.com.co>
2023-09-04 09:21:08 -0500

    Creating a Spring MVC web controller

---
+++ src/main/java/dev/cgomezu/learningspringboot3/HomeController.java
@@ -0,0 +1,13 @@
+package dev.cgomezu.learningspringboot3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
